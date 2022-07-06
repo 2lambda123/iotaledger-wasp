37316fe577a64dbb525632ae98957b603697fcf2
Ivange Larry <ivangelarry@gmail.com>
2022-07-06 22:46:57 +0100

    Add memory contract wasm

---
+++ contracts/wasm/memory/test/memory_test.go
@@ -0,0 +1,31 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package test
+
+import (
+	"testing"
+
+	"github.com/iotaledger/wasp/contracts/wasm/memory/go/memory"
+	"github.com/iotaledger/wasp/packages/wasmvm/wasmlib/go/wasmlib"
+	"github.com/iotaledger/wasp/packages/wasmvm/wasmsolo"
+	"github.com/stretchr/testify/require"
+)
+
+func deployContract(t *testing.T) *wasmsolo.SoloContext {
+	ctx := wasmsolo.NewSoloContext(t, memory.ScName, memory.OnLoad)
+	require.NoError(t, ctx.ContractExists(memory.ScName))
+	return ctx
+}
+
+func TestCallF(t *testing.T) {
+	wasmlib.ConnectHost(nil)
+	ctx := deployContract(t)
+	f := memory.ScFuncs.F(ctx)
+	for i := uint32(1000); i <= 100000; i += 1000 {
+		f.Params.N().SetValue(i)
+		f.Func.Post()
+		require.NoError(t, ctx.Err)
+		t.Logf("n = %d, gas = %d\n", i, ctx.Gas)
+	}
+}
