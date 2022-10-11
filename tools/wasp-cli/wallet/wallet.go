6a4d5b0c53e0c3606c317020e2c17169322a2066
Ivange Larry <ivangelarry@gmail.com>
2022-10-11 15:00:23 +0100

    jsonify cli

--- tools/wasp-cli/wallet/wallet.go
+++ tools/wasp-cli/wallet/wallet.go
@@ -19,6 +19,11 @@ type Wallet struct {
 	KeyPair *cryptolib.KeyPair
 }
 
+type BalanceModel struct {
+	Address string
+	Balance map[string]interface{}
+}
+
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a new wallet",
