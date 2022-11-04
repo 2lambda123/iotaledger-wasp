55a02241b8c7a25dec4d5a496dd00bbf93f4bc83
Yang Hau <yuanyanghau@gmail.com>
2022-11-04 12:30:13 +0530

    chore: Remove viper dependency

--- contracts/wasm/testwasmlib/test/testwasmlib_client_test.go
+++ contracts/wasm/testwasmlib/test/testwasmlib_client_test.go
@@ -1,13 +1,14 @@
 package test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/mr-tron/base58"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 
 	"github.com/iotaledger/wasp/client/chainclient"
@@ -68,17 +69,27 @@ func setupClientCluster(t *testing.T) *wasmclient.WasmClientContext {
 }
 
 func setupClientDisposable(t *testing.T) *wasmclient.WasmClientContext {
-	viper.SetConfigFile("wasp-cli.json")
-	err := viper.ReadInConfig()
+	type jsonConfigFile struct {
+		Chain struct {
+			Chain string `json:"chain"`
+		} `json:"chain"`
+		Wallet struct {
+			Seed string `json:"seed"`
+		} `json:"wallet"`
+	}
+	f, err := os.Open("wasp-cli.json")
 	require.NoError(t, err)
+	b, err := io.ReadAll(f)
+	require.NoError(t, err)
+	var c jsonConfigFile
+	json.Unmarshal(b, &c)
 
-	chain := viper.GetString("chains." + viper.GetString("chain"))
-	chID, err := isc.ChainIDFromString(chain)
+	chID, err := isc.ChainIDFromString(c.Chain.Chain)
 	require.NoError(t, err)
 	chainID := wasmtypes.ChainIDFromBytes(chID.Bytes())
 
 	// we'll use the seed keypair to sign requests
-	seedBytes, err := base58.Decode(viper.GetString("wallet.seed"))
+	seedBytes, err := base58.Decode(c.Wallet.Seed)
 	require.NoError(t, err)
 	seed := cryptolib.NewSeedFromBytes(seedBytes)
 	wallet := cryptolib.NewKeyPairFromSeed(seed.SubSeed(0))
