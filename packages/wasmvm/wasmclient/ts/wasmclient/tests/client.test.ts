3055ad55164b532e95335c16718dd42b5e3b0728
Lukas Möller <lukas.moeller@iota.org>
2022-11-07 15:47:23 +0100

    Use async test in wasmlib

--- packages/wasmvm/wasmclient/ts/wasmclient/tests/client.test.ts
+++ packages/wasmvm/wasmclient/ts/wasmclient/tests/client.test.ts
@@ -5,9 +5,9 @@ const nodeUrl = 'https://chrysalis-nodes.iota.org';
 describe('clientLib', function () {
     const clientLib = new ClientLib(nodeUrl);
 
-    describe('get node health', function () {
-        it('should return node health', () => {
-            const isHealthy = clientLib.isHealthy();
+    describe('get node health', function  () {
+        it('should return node health', async () => {
+            const isHealthy = await clientLib.isHealthy();
             expect(isHealthy).toBeTruthy();
         });
     });
