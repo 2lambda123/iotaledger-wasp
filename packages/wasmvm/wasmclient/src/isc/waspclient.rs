62c2f59d36aefa28e995d291b62fdf529116da39
Yang Hau <yuanyanghau@gmail.com>
2022-11-21 12:43:15 +0700

    Add tests for waspclient (#1434)
    * use errors::Result
    
    * add test for waspclient

--- packages/wasmvm/wasmclient/src/isc/waspclient.rs
+++ packages/wasmvm/wasmclient/src/isc/waspclient.rs
@@ -26,14 +26,13 @@ impl WaspClient {
         chain_id: &ScChainID,
         contract_hname: &ScHname,
         function_hname: &ScHname,
-        args: ScDict,
+        args: &ScDict,
         optimistic_read_timeout: Option<Duration>,
     ) -> errors::Result<()> {
         let deadline = match optimistic_read_timeout {
             Some(duration) => duration,
             None => DEFAULT_OPTIMISTIC_READ_TIMEOUT,
         };
-
         let url = format!(
             "{}/chain/{}/contract/{}/callviewbyhname/{}",
             self.base_url,
@@ -41,11 +40,13 @@ impl WaspClient {
             contract_hname.to_string(),
             function_hname.to_string()
         );
+
         let client = reqwest::blocking::Client::builder()
             .timeout(deadline)
             .build()
             .unwrap();
         let _ = client.post(url).body(args.to_bytes()).send();
+
         Ok(())
     }
     pub fn post_offledger_request(
@@ -76,5 +77,53 @@ impl WaspClient {
             .unwrap();
         let _ = client.get(url).send();
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::waspclient;
+    use httpmock::prelude::*;
+
+    #[test]
+    fn waspclient_new() {
+        let client = waspclient::WaspClient::new("http://localhost");
+        assert!(client.base_url == "http://localhost");
+    }
+
+    #[test]
+    fn test_call_view_by_hname() {
+        let chain_id_bytes = vec![
+            41, 180, 220, 182, 186, 38, 166, 60, 91, 105, 181, 183, 219, 243, 200, 162, 131, 181,
+            57, 142, 41, 30, 236, 92, 178, 1, 116, 229, 174, 86, 156, 210,
+        ];
+        let chain_id_str = "tgl1pq5mfh9khgn2v0zmdx6m0klnez3g8dfe3c53amzukgqhfedw26wdy8tztdy";
+        let contract_hname = "testwasmlib";
+        let function_hname = "tokenBalance";
+
+        let mock_server = MockServer::start();
+        let call_view_by_hname_mock = mock_server.mock(|when, then| {
+            when.method(POST).path(format!(
+                "/chain/{}/contract/{}/callviewbyhname/{}",
+                chain_id_str, contract_hname, function_hname
+            ));
+            then.status(200);
+        });
+
+        let client = waspclient::WaspClient::new(&mock_server.base_url());
+        let sc_chain_id = wasmlib::chain_id_from_bytes(&chain_id_bytes);
+        let sc_contract_hname = wasmlib::hname_from_bytes(&wasmlib::uint32_to_bytes(0x89703a45));
+        let sc_function_hname = wasmlib::hname_from_bytes(&wasmlib::uint32_to_bytes(0x78cc397a));
+        let args = wasmlib::ScDict::new(&vec![]);
+        let _ = client
+            .call_view_by_hname(
+                &sc_chain_id,
+                &sc_contract_hname,
+                &sc_function_hname,
+                &args,
+                None,
+            )
+            .unwrap();
+        call_view_by_hname_mock.assert();
     }
 }
