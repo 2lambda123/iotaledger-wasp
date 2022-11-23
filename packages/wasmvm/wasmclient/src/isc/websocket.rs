50aa9f52cdcfb45e0fdd11bf970076d5c1e9fd8e
Yang Hau <yuanyanghau@gmail.com>
2022-11-24 02:14:26 +0800

    test: Amend ws mock server

--- packages/wasmvm/wasmclient/src/isc/websocket.rs
+++ packages/wasmvm/wasmclient/src/isc/websocket.rs
@@ -1,19 +1,24 @@
 use crate::*;
-// use tungstenite::*;
+use std::net;
+use tungstenite::*;
 
-#[derive(Clone)]
+pub type SocketType =
+    tungstenite::WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>;
+
 struct Client {
     url: String,
+    socket: SocketType,
 }
 
 impl Client {
     pub fn connect(url: &str) -> errors::Result<Self> {
-        let client = Client {
-            url: url.to_owned(),
-        };
-
-        match tungstenite::connect(&client.url) {
-            Ok(_v) => return Ok(client),
+        match tungstenite::connect(url) {
+            Ok((socket, _res)) => {
+                return Ok(Client {
+                    url: url.to_owned(),
+                    socket: socket,
+                })
+            }
             Err(e) => return Err(e.to_string()),
         }
     }
@@ -25,26 +30,45 @@ impl Client {
 #[cfg(test)]
 mod tests {
     use crate::websocket::Client;
-    use std::{net::TcpListener, thread::spawn};
+    use std::{clone, net::TcpListener, ops::Index, thread::spawn};
     use tungstenite::{
-        accept, accept_hdr,
+        accept,
         handshake::server::{Request, Response},
     };
 
     #[test]
     fn client_connect() {
-        let url = "localhost:3012";
-        spawn(move || mock_server(url));
+        let url = "ws://localhost:3012";
+        mock_server(url, None);
         let client = Client::connect(&format!("ws://{}", url)).unwrap();
         assert!(client.url == format!("ws://{}", url));
     }
 
-    fn mock_server(url: &str) {
-        let server = TcpListener::bind(url).unwrap();
-        for stream in server.incoming() {
-            spawn(move || {
-                accept(stream.unwrap()).unwrap();
-            });
-        }
+    #[test]
+    fn client_subscribe() {
+        let url = "ws://localhost:3012";
+        mock_server(url, Some("hi"));
+        let client = Client::connect(&format!("ws://{}", url)).unwrap();
+    }
+
+    fn mock_server(input_url: &str, response_msg: Option<&str>) {
+        let ws_prefix = "ws://";
+        let url = match input_url.to_string().strip_prefix(ws_prefix) {
+            Some(u) => u.to_string(),
+            None => input_url.to_string(),
+        };
+        let msg = match response_msg {
+            Some(m) => m.to_string(),
+            None => "".to_string(),
+        };
+        spawn(move || {
+            let server = TcpListener::bind(url).unwrap();
+            for stream in server.incoming() {
+                let mut socket = accept(stream.unwrap()).unwrap();
+                if !msg.is_empty() {
+                    socket.write_message(msg.clone().into()).unwrap();
+                }
+            }
+        });
     }
 }
