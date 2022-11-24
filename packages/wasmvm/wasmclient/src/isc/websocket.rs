3418a7fab4eb97b6c8a81bd228702d17236d3355
Yang Hau <yuanyanghau@gmail.com>
2022-11-24 19:02:02 +0800

    feat: Implement subscribe for wasmclient in rust

--- packages/wasmvm/wasmclient/src/isc/websocket.rs
+++ packages/wasmvm/wasmclient/src/isc/websocket.rs
@@ -1,6 +1,5 @@
 use crate::*;
-use std::net;
-use tungstenite::*;
+use std::sync::mpsc;
 
 pub type SocketType =
     tungstenite::WebSocket<tungstenite::stream::MaybeTlsStream<std::net::TcpStream>>;
@@ -19,54 +18,89 @@ impl Client {
                     socket: socket,
                 })
             }
-            Err(e) => return Err(e.to_string()),
+            Err(e) => return Err(format!("websocket connection err: {}", e)),
         }
     }
-    pub fn subscribe() -> errors::Result<()> {
-        Ok(())
+    pub fn subscribe(&mut self, ch: mpsc::Sender<String>) -> errors::Result<()> {
+        loop {
+            match self.socket.read_message() {
+                Ok(msg) => {
+                    if msg.to_string() != "" {
+                        ch.send(msg.to_string()).unwrap();
+                    }
+                }
+                Err(tungstenite::Error::ConnectionClosed) => {
+                    return Ok(());
+                }
+                Err(e) => {
+                    return Err(format!("subscribe err: {}", e));
+                }
+            }
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::websocket::Client;
-    use std::{clone, net::TcpListener, ops::Index, thread::spawn};
-    use tungstenite::{
-        accept,
-        handshake::server::{Request, Response},
-    };
+    use std::{net::TcpListener, sync::mpsc, thread::spawn};
+    use tungstenite::accept;
+
+    #[derive(Clone)]
+    struct MockServerMsg {
+        msg: String,
+        count: u32,
+    }
 
     #[test]
     fn client_connect() {
         let url = "ws://localhost:3012";
         mock_server(url, None);
-        let client = Client::connect(&format!("ws://{}", url)).unwrap();
-        assert!(client.url == format!("ws://{}", url));
+        let client = Client::connect(&url).unwrap();
+        assert!(client.url == url);
     }
 
     #[test]
     fn client_subscribe() {
         let url = "ws://localhost:3012";
-        mock_server(url, Some("hi"));
-        let client = Client::connect(&format!("ws://{}", url)).unwrap();
+        let test_msg = "this is test msg";
+        mock_server(
+            url,
+            Some(MockServerMsg {
+                msg: test_msg.to_string(),
+                count: 3,
+            }),
+        );
+        let mut client = Client::connect(&url).unwrap();
+        let (tx, rx): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
+        client.subscribe(tx).unwrap();
+        let mut cnt = 0;
+        for msg in rx.iter() {
+            assert!(msg == format!("{}: cnt: {}", test_msg, cnt));
+            cnt += 1;
+        }
     }
 
-    fn mock_server(input_url: &str, response_msg: Option<&str>) {
+    fn mock_server(input_url: &str, response_msg: Option<MockServerMsg>) {
         let ws_prefix = "ws://";
         let url = match input_url.to_string().strip_prefix(ws_prefix) {
-            Some(u) => u.to_string(),
+            Some(v) => v.to_string(),
             None => input_url.to_string(),
-        };
-        let msg = match response_msg {
-            Some(m) => m.to_string(),
-            None => "".to_string(),
         };
         spawn(move || {
             let server = TcpListener::bind(url).unwrap();
             for stream in server.incoming() {
                 let mut socket = accept(stream.unwrap()).unwrap();
-                if !msg.is_empty() {
-                    socket.write_message(msg.clone().into()).unwrap();
+                let msg_opt = response_msg.to_owned();
+                if !msg_opt.is_none() {
+                    let msg = msg_opt.unwrap();
+                    for cnt in 0..msg.count {
+                        let msg = format!("{}: cnt: {}", msg.msg, cnt);
+                        socket
+                            .write_message(tungstenite::Message::Text(msg).into())
+                            .unwrap();
+                    }
+                    socket.close(None).unwrap();
                 }
             }
         });
