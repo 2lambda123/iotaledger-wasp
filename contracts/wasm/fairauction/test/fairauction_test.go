23bd9839738972ed167fa49fe00cb092d5c100e7
Eric Hop <eric@iota.org>
2022-12-05 09:03:04 -0800

    fix build

--- contracts/wasm/fairauction/test/fairauction_test.go
+++ contracts/wasm/fairauction/test/fairauction_test.go
@@ -23,7 +23,6 @@ const (
 )
 
 func startAuction(t *testing.T) (*wasmsolo.SoloContext, *wasmsolo.SoloAgent, wasmtypes.ScNftID) {
-	*wasmsolo.TsWasm = true
 	ctx := wasmsolo.NewSoloContext(t, fairauction.ScName, fairauctionimpl.OnDispatch)
 	auctioneer := ctx.NewSoloAgent()
 	nftID := ctx.MintNFT(auctioneer, []byte("NFT metadata"))
