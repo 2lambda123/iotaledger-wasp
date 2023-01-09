tadash10/wasp

Update docs, routes, groups

plugins/webapi/component.go
@@ -117,9 +117,10 @@ func initWebAPI() {
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowMethods: []string{"*"},
 	}))
-	Server.SetExternalDocs("Find out more about Swagger", "http://swagger.io").
+	Server.AddSecurityAPIKey("Authorization", "JWT Token", echoswagger.SecurityInHeader).
+		SetExternalDocs("Find out more about Wasp", "https://wiki.iota.org/smart-contracts/overview").
 		SetResponseContentType("application/xml", "application/json").
-		SetUI(echoswagger.UISetting{DetachSpec: true, HideTop: true}).
+		SetUI(echoswagger.UISetting{DetachSpec: false, HideTop: false}).
 		SetScheme("http", "https")
 
 	network := peering.DefaultNetworkProvider()

2lambda123/iotaledger-wasp

plugins/webapi/component.go
@@ -7,10 +7,6 @@ import (
 	"net/http"
 	"time"
 
-	v1 "github.com/iotaledger/wasp/packages/webapi/v1"
-	"github.com/iotaledger/wasp/packages/webapi/v1/httperrors"
-	v2 "github.com/iotaledger/wasp/packages/webapi/v2"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/pangpanglabs/echoswagger/v2"
@@ -135,10 +131,11 @@ func provide(c *dig.Container) error {
 			Version:     wasp.Version,
 		})
 
-		echoSwagger.SetExternalDocs("Find out more about Swagger", "http://swagger.io").
+		echoSwagger.AddSecurityAPIKey("Authorization", "JWT Token", echoswagger.SecurityInHeader).
+			SetExternalDocs("Find out more about Wasp", "https://wiki.iota.org/smart-contracts/overview").
 			SetResponseContentType("application/xml", "application/json").
-			SetUI(echoswagger.UISetting{DetachSpec: true, HideTop: true}).
-			SetScheme("https", "http")
+			SetUI(echoswagger.UISetting{DetachSpec: false, HideTop: false}).
+			SetScheme("http", "https")
 
 		v1.Init(
 			Plugin.App().NewLogger("WebAPI/v1"),