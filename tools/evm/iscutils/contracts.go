d0b52c2e894ea3545f672f37fff9111a38f9db92
cwarnerdev <138500512+cwarnerdev@users.noreply.github.com>
2023-10-31 04:43:14 -1000

    move build products to own directory

--- tools/evm/iscutils/contracts.go
+++ tools/evm/iscutils/contracts.go
@@ -14,28 +14,28 @@ import (
 // the `solc` binary installed in your system. Then, simply run `go generate`
 // in this directory.
 
-//go:generate solc --abi --bin --bin-runtime --overwrite prng_test.sol -o .
+//go:generate solc --abi --bin --bin-runtime --overwrite prng_test.sol -o build/.
 var (
-	//go:embed PRNGTest.abi
+	//go:embed build/PRNGTest.abi
 	PRNGTestContractABI string
-	//go:embed PRNGTest.bin
+	//go:embed build/PRNGTest.bin
 	PRNGTestContractBytecodeHex string
 	PRNGTestContractBytecode    = common.FromHex(strings.TrimSpace(PRNGTestContractBytecodeHex))
 	//deployed bytecode and runtime bytecode are different, see: https://ethereum.stackexchange.com/questions/13086/whats-the-difference-between-solcs-bin-bytecode-versus-bin-runtime
-	//go:embed PRNGTest.bin-runtime
+	//go:embed build/PRNGTest.bin-runtime
 	PRNGTestContractRuntimeBytecodeHex string
 	PRNGTestContractRuntimeBytecode    = common.FromHex(strings.TrimSpace(PRNGTestContractRuntimeBytecodeHex))
 )
 
-//go:generate solc --abi --bin --bin-runtime --overwrite nft_test.sol @iota=../../../packages/vm/core/evm -o .
+//go:generate solc --abi --bin --bin-runtime --overwrite nft_test.sol @iota=../../../packages/vm/core/evm -o build/.
 var (
-	//go:embed NFTTest.abi
+	//go:embed build/NFTTest.abi
 	NFTTestContractABI string
-	//go:embed NFTTest.bin
+	//go:embed build/NFTTest.bin
 	NFTTestContractBytecodeHex string
 	NFTTestContractBytecode    = common.FromHex(strings.TrimSpace(NFTTestContractBytecodeHex))
 	//deployed bytecode and runtime bytecode are different, see: https://ethereum.stackexchange.com/questions/13086/whats-the-difference-between-solcs-bin-bytecode-versus-bin-runtime
-	//go:embed NFTTest.bin-runtime
+	//go:embed build/NFTTest.bin-runtime
 	NFTTestContractRuntimeBytecodeHex string
 	NFTTestContractRuntimeBytecode    = common.FromHex(strings.TrimSpace(NFTTestContractRuntimeBytecodeHex))
 )
