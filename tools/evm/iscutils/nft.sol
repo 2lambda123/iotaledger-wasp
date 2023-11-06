1d0327f8d9648eec20a8bbc6ecfe29e0a039dbb8
cwarnerdev <138500512+cwarnerdev@users.noreply.github.com>
2023-11-06 04:09:06 -1000

    wip

--- tools/evm/iscutils/nft.sol
+++ tools/evm/iscutils/nft.sol
@@ -4,33 +4,49 @@ pragma solidity >=0.8.5;
 
 import "@iota/iscmagic/ISC.sol";
 
-library nft {
+library NFT {
+    /// @notice Mints an NFT with ImmutableData that will be owned by the AgentID.
+    /// It's possible to mint as part of a collection (the caller must be the owner
+    /// of the collection NFT to mint new NFTs as part of said collection).
+    /// The mint can be done directly to any L1 address (it is not necessary for
+    /// the target to have an account on the chain).
+    /// @param immutableData Immutable data for the NFT
+    /// @param agentID The AgentID that will be the owner of the NFT
+    /// @param nftID CollectionID (NFTID) for the NFT
+    /// @param withdrawal Whether to withdrawal the NFT in the minting step
+    /// (can only be true when the targetAgentID is a L1 address)
+    /// @return MintID the internal ID of the NFT at the time of minting that can be
+    /// used by users/contracts to obtain the resulting NFTID on the next block
     function MintNFT(
         bytes memory immutableData,
         ISCAgentID memory agentID,
         NFTID nftID,
-        bool withdrawal
-    ) public {
+        bool withdrawal,
+        uint64 allowance
+    ) public returns (bytes6 MintID) {
         ISCDict memory params = ISCDict(new ISCDictItem[](4));
-        // ([]byte): ImmutableData for the NFT.
         params.items[0] = ISCDictItem("I", immutableData);
-        // (AgentID): AgentID that will be the owner of the NFT.
         params.items[1] = ISCDictItem("a", agentID.data);
-        // (optional NFTID - default empty): collectionID (NFTID) for the NFT.
         params.items[2] = ISCDictItem("C", abi.encodePacked(nftID));
-        // (optional bool - default false): whether to withdrawal the NFT in the minting step
-        // (can only be true when the targetAgentID is a L1 address).
         params.items[3] = ISCDictItem("W", boolToBytes(withdrawal));
 
-        ISCAssets memory allowance;
-        ISC.sandbox.call(
+        ISCAssets memory assets= makeAllowanceBaseTokens(allowance);
+
+        ISCDict memory returnedDict = ISC.sandbox.call(
             ISC.util.hn("accounts"),
             ISC.util.hn("mintNFT"),
             params,
-            allowance
+            assets
         );
-    }
 
+        ISCDictItem memory item = returnedDict.items[0];
+        return bytes6(item.value);
+    }
+    function makeAllowanceBaseTokens(uint64 amount) internal pure returns (ISCAssets memory) {
+        ISCAssets memory assets;
+        assets.baseTokens = amount;
+        return assets;
+    }
     function boolToBytes(bool b) internal pure returns (bytes memory) {
         return abi.encodePacked(b ? uint8(1) : uint8(0));
     }
--- tools/evm/iscutils/nft_test.sol
+++ tools/evm/iscutils/nft_test.sol
@@ -2,19 +2,65 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.5;
 
-import "@iota/iscmagic/ISCTypes.sol";
+import "@iota/iscmagic/ISC.sol";
 import "./nft.sol";
 
 contract NFTTest {
-    using nft for *;
+    using NFT for *;
+    event Minted(bytes6 mintID);
+
+    bytes public constant immutableData = "0xabc123";
+    ISCAgentID public agentID = ISCAgentID({data: abi.encodePacked(msg.sender)});
+    NFTID public constant nftID = NFTID.wrap(bytes32(0));
+    bool public constant withdrawal = false;
 
     // Testing the MintNFT function
-    function MintNFT(
+    function MintTestNFT() public {
+        require(true == false,"kaboom");
+        bytes6 mintID = NFT.MintNFT(immutableData, agentID, nftID, withdrawal);
+
+        require(bytes32(mintID) != bytes32(0),"empty MintID returned");
+       // mintID = NFT.MintNFT(immutableData, agentID, nftID, withdrawal);
+       // require(mintID.length == 0,"empty MintID returned");
+        emit Minted(mintID);
+    }
+
+    function MintNFTWithReturnedDict() public returns (ISCDict memory) {
+
+        ISCDict memory params = ISCDict(new ISCDictItem[](4));
+        params.items[0] = ISCDictItem("I", immutableData);
+        params.items[1] = ISCDictItem("a", agentID.data);
+        params.items[2] = ISCDictItem("C", abi.encodePacked(nftID));
+        params.items[3] = ISCDictItem("W", boolToBytes(withdrawal));
+
+        ISCAssets memory allowance = makeAllowanceBaseTokens(1000000);
+
+       return ISC.sandbox.call(
+            ISC.util.hn("accounts"),
+            ISC.util.hn("mintNFT"),
+            params,
+            allowance
+        );
+
+
+    }
+    function makeAllowanceBaseTokens(uint64 amount) private pure returns (ISCAssets memory) {
+        ISCAssets memory assets;
+        assets.baseTokens = amount;
+        return assets;
+    }
+    function boolToBytes(bool b) internal pure returns (bytes memory) {
+        return abi.encodePacked(b ? uint8(1) : uint8(0));
+    }
+    /*
+    function MintCustomNFT(
         bytes memory immutableData,
         ISCAgentID memory agentID,
         NFTID nftID,
         bool withdrawal
     ) public {
-        nft.MintNFT(immutableData, agentID, nftID, withdrawal);
+        bytes memory mintID = NFT.MintNFT(immutableData, agentID, nftID, withdrawal);
+        emit Minted(mintID);
     }
+    */
 }
