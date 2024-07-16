7bd9338aa86e19cfdbc02a698c862b399b5746dd
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-07-16 16:59:04 +0530

    merge allowance and takeAllowance contract

--- packages/evm/evmtest/wiki_how_tos/TakeAllowance.sol
+++ packages/evm/evmtest/wiki_how_tos/TakeAllowance.sol
@@ -4,7 +4,15 @@ pragma solidity ^0.8.0;
 
 import "@iscmagic/ISC.sol";
 
-contract allowance {
+contract Allowance {
+    function allow(address _address, bytes32 _allowanceNFTID) public {
+        NFTID[] memory nftIDs = new NFTID[](1);
+        nftIDs[0] = NFTID.wrap(_allowanceNFTID);
+        ISCAssets memory assets;
+        assets.nfts = nftIDs;
+        ISC.sandbox.allow(_address, assets);
+    }
+
     function takeAllowedFunds(
         address _address,
         bytes32 _allowanceNFTID
